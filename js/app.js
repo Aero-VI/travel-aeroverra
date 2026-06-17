35f9a846a2272ab3bed0f2387a3ef6e4c9b0460f
Azula <azula@aero.vi>
2026-06-17 14:23:29 -0400

    feat: add Liberty of the Seas cruise + CPH-LHR flight, add geocodes for LHR/Hamburg/Zeebrugge, cache bust

--- js/app.js
+++ js/app.js
@@ -1849,7 +1849,7 @@ function setupSearch() {
 async function init() {
     try {
         const [tripsRes, eventsRes] = await Promise.all([
-            fetch('data/trips.json?h=26cb2904'),
+            fetch('data/trips.json?h=d40bda3b'),
             fetch('data/events.json?h=6c07e64f')
         ]);
         tripsData = await tripsRes.json();
